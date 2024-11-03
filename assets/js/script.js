36a2988d6ffe5ae68bd244797adb88c24df797e6
Professor Igor <trabalho.computador.sjn@gmail.com>
2024-11-03 14:15:41 -0300

    feat: gerador de qrcode online

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+const qrCode = new QRCodeStyling({
+    width: 300,
+    height: 300,
+    type: "svg",
+    data: "",
+    image: "https://www.sophialabs.com.br/assets/img/logo_sophia.png",
+    dotsOptions: {
+        color: "#000",
+        type: "rounded"
+    },
+    backgroundOptions: {
+        color: "#ffffff",
+    },
+    imageOptions: {
+        crossOrigin: "anonymous",
+        margin: 20
+    }
+});
+
+function generateQRCode() {
+    const text = document.getElementById("text").value;
+    if (text) {
+        qrCode.update({
+            data: text
+        });
+
+        qrCode.append(document.getElementById("canvas"));
+
+        const downloadLink = document.getElementById("downloadLink");
+        downloadLink.style.display = "inline";
+        downloadLink.onclick = () => qrCode.download({ name: "qr_code", extension: "png" });
+    } else {
+        alert("Por favor, insira algum texto ou URL.");
+    }
+}
