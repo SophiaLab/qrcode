4e4e139d229ea293457b9a6e2bd19342b2fd1214
igor Muniz <trabalho.computador.sjn@gmail.com>
2025-03-18 16:21:37 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const qrCode = new QRCodeStyling({
     height: 300,
     type: "svg",
     data: "",
-    image: "https://www.sophialabs.com.br/assets/img/logo_sophia.png",
+    image: "https://github.com/SophiaLab/qrcode/blob/main/assets/img/logo_sophia.png",
     dotsOptions: {
         color: "#000",
         type: "rounded"
